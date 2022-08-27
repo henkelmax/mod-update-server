9f26db4e933a6b6306ccc136791b6c7e7cb53de8
Max Henkel <max@maxhenkel.de>
2022-08-27 16:37:57 +0200

    Fix webpack chunk name

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -14,10 +14,7 @@ export default new Router({
     {
       path: '/mods',
       name: 'mods',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Mods.vue')
+      component: () => import(/* webpackChunkName: "mods" */ './views/Mods.vue')
     },
     {
       path: '/mod',
