30bd55f5b805413fc3a05df221d39d5c317c161c
Max Henkel <max@maxhenkel.de>
2023-12-03 13:24:32 +0100

    Add prometheus actuator

--- backend/src/main/java/de/maxhenkel/modupdateserver/controllers/ForgeController.java
+++ backend/src/main/java/de/maxhenkel/modupdateserver/controllers/ForgeController.java
@@ -1,87 +1,45 @@
 package de.maxhenkel.modupdateserver.controllers;
 
-import de.maxhenkel.modupdateserver.entities.Mod;
-import de.maxhenkel.modupdateserver.entities.Update;
-import de.maxhenkel.modupdateserver.repositories.ModRepository;
+import de.maxhenkel.modupdateserver.services.ForgeUpdateService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.AggregationResults;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.server.ResponseStatusException;
-import org.thymeleaf.util.StringUtils;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
 @Controller
 public class ForgeController {
 
     @Autowired
-    private ModRepository modRepository;
+    private ForgeUpdateService forgeUpdateService;
 
-    @Autowired
-    MongoTemplate mongoTemplate;
+    private final Counter totalUpdateCheckCounter;
+    private final Counter forgeUpdateCheckCounter;
+    private final Counter neoforgeUpdateCheckCounter;
 
-    @Cacheable(value = "forge", cacheManager = "cacheManager")
-    @GetMapping("/forge/{modID}")
-    public ResponseEntity<?> forgModUpdates(@PathVariable("modID") String modID) {
-        return modUpdatesForLoader("forge", modID);
+    public ForgeController(MeterRegistry meterRegistry) {
+        totalUpdateCheckCounter = Counter.builder("update_check.total")
+                .register(meterRegistry);
+        forgeUpdateCheckCounter = Counter.builder("update_check.loader.forge")
+                .register(meterRegistry);
+        neoforgeUpdateCheckCounter = Counter.builder("update_check.loader.neoforge")
+                .register(meterRegistry);
     }
 
-    @Cacheable(value = "neoforge", cacheManager = "cacheManager")
+    @GetMapping("/forge/{modID}")
+    public ResponseEntity<?> forgeModUpdates(@PathVariable("modID") String modID) {
+        totalUpdateCheckCounter.increment();
+        forgeUpdateCheckCounter.increment();
+        return forgeUpdateService.modUpdatesForLoader("forge", modID);
+    }
+
     @GetMapping("/neoforge/{modID}")
     public ResponseEntity<?> neoForgeModUpdates(@PathVariable("modID") String modID) {
-        return modUpdatesForLoader("neoforge", modID);
-    }
-
-    private ResponseEntity<?> modUpdatesForLoader(String loader, String modID) {
-        Optional<Mod> optionalMod = modRepository.findByModID(modID);
-        if (optionalMod.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Mod does not exist");
-        }
-        Mod mod = optionalMod.get();
-
-        Map<String, Object> forgeFormat = new HashMap<>();
-        Map<String, String> promos = new HashMap<>();
-
-        Aggregation agg = newAggregation(
-                match(Criteria.where("mod").is(mod.getId())),
-                match(Criteria.where("modLoader").is(loader)),
-                sort(Sort.Direction.ASC, "publishDate")
-        );
-
-        AggregationResults<Update> aggregate = mongoTemplate.aggregate(agg, Update.class, Update.class);
-
-        for (Update update : aggregate.getMappedResults()) {
-            Map<String, String> o = (Map<String, String>) forgeFormat.get(update.getGameVersion());
-            if (o == null) {
-                o = new HashMap<>();
-                forgeFormat.put(update.getGameVersion(), o);
-            }
-
-            o.put(update.getVersion(), StringUtils.join(update.getUpdateMessages(), "\n"));
-
-            if (Arrays.asList(update.getTags()).contains("recommended")) {
-                promos.put("%s-recommended".formatted(update.getGameVersion()), update.getVersion());
-            }
-            promos.put("%s-latest".formatted(update.getGameVersion()), update.getVersion());
-        }
-
-        forgeFormat.put("promos", promos);
-        forgeFormat.put("homepage", mod.getWebsiteURL());
-        return new ResponseEntity<>(forgeFormat, HttpStatus.OK);
+        totalUpdateCheckCounter.increment();
+        neoforgeUpdateCheckCounter.increment();
+        return forgeUpdateService.modUpdatesForLoader("neoforge", modID);
     }
 
 }
---
+++ backend/src/main/java/de/maxhenkel/modupdateserver/services/ForgeUpdateService.java
@@ -0,0 +1,84 @@
+package de.maxhenkel.modupdateserver.services;
+
+import de.maxhenkel.modupdateserver.entities.Mod;
+import de.maxhenkel.modupdateserver.entities.Update;
+import de.maxhenkel.modupdateserver.repositories.ModRepository;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+
+@Service
+public class ForgeUpdateService {
+
+    @Autowired
+    private ModRepository modRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    private final Counter updateCheckCacheMissCounter;
+
+    public ForgeUpdateService(MeterRegistry meterRegistry) {
+        updateCheckCacheMissCounter = Counter.builder("update_check.cache.cache_miss")
+                .register(meterRegistry);
+    }
+
+    @Cacheable(value = "mod_updates", cacheManager = "cacheManager")
+    public ResponseEntity<?> modUpdatesForLoader(String loader, String modID) {
+        updateCheckCacheMissCounter.increment();
+        Optional<Mod> optionalMod = modRepository.findByModID(modID);
+        if (optionalMod.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Mod does not exist");
+        }
+        Mod mod = optionalMod.get();
+
+        Map<String, Object> forgeFormat = new HashMap<>();
+        Map<String, String> promos = new HashMap<>();
+
+        Aggregation agg = newAggregation(
+                match(Criteria.where("mod").is(mod.getId())),
+                match(Criteria.where("modLoader").is(loader)),
+                sort(Sort.Direction.ASC, "publishDate")
+        );
+
+        AggregationResults<Update> aggregate = mongoTemplate.aggregate(agg, Update.class, Update.class);
+
+        for (Update update : aggregate.getMappedResults()) {
+            Map<String, String> o = (Map<String, String>) forgeFormat.get(update.getGameVersion());
+            if (o == null) {
+                o = new HashMap<>();
+                forgeFormat.put(update.getGameVersion(), o);
+            }
+
+            o.put(update.getVersion(), StringUtils.join(update.getUpdateMessages(), "\n"));
+
+            if (Arrays.asList(update.getTags()).contains("recommended")) {
+                promos.put("%s-recommended".formatted(update.getGameVersion()), update.getVersion());
+            }
+            promos.put("%s-latest".formatted(update.getGameVersion()), update.getVersion());
+        }
+
+        forgeFormat.put("promos", promos);
+        forgeFormat.put("homepage", mod.getWebsiteURL());
+        return new ResponseEntity<>(forgeFormat, HttpStatus.OK);
+    }
+
+}
