5c036bbb419c7e8dd50acf48d4e35adb91c9a6b9
Max Henkel <max@maxhenkel.de>
2023-12-06 21:49:13 +0100

    Add search bar to mods list

--- frontend/src/views/Mods.vue
+++ frontend/src/views/Mods.vue
@@ -10,8 +10,19 @@
       <v-btn text color="white" @click="router.push('apikeys')"> API Keys </v-btn>
       <v-btn text color="white" @click="doLogout"> Log Out </v-btn>
     </v-toolbar>
+    <v-text-field
+      class="mx-4"
+      :loading="false"
+      v-model="searchText"
+      variant="underlined"
+      label="Search Mods"
+      append-inner-icon="mdi-magnify"
+      single-line
+      hide-details
+      flat
+    ></v-text-field>
     <v-list lines="two">
-      <template v-for="(mod, i) in mods" :key="mod.modID">
+      <template v-for="(mod, i) in filteredMods" :key="mod.modID">
         <v-list-item
           :title="mod.name"
           :subtitle="mod.description"
@@ -35,6 +46,9 @@
         <v-divider v-if="i !== mods.length - 1" class="ml-2 mr-2"></v-divider>
       </template>
     </v-list>
+    <v-card-text v-if="filteredMods.length <= 0" class="text-center">
+      There are no mods matching your search
+    </v-card-text>
   </v-card>
   <v-container>
     <v-dialog v-model="dialog" max-width="35%">
@@ -58,12 +72,13 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, computed } from "vue";
 import { getMods, deleteMod, getErrorMessage } from "@/services";
 import { logout } from "@/stores/application";
 import router from "@/router";
 
 const mods = ref([]);
+const searchText = ref("");
 const dialog = ref(false);
 const modToDelete = ref({});
 const snackbar = ref(false);
@@ -71,6 +86,12 @@ const error = ref("");
 
 onMounted(async () => {
   updateMods();
+});
+
+const filteredMods = computed(() => {
+  return mods.value.filter((m) =>
+    m.name.toLowerCase().includes(searchText.value.toLowerCase())
+  );
 });
 
 function updateMods() {
