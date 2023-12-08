68be96a018265d98f8d113d08b28205c2eb3052a
Max Henkel <max@maxhenkel.de>
2023-12-08 15:37:50 +0100

    Use hash history mode in frontend

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Default from '@/layouts/default/Default.vue'
 import Login from '@/views/Login.vue'
 import Mods from '@/views/Mods.vue'
@@ -73,7 +73,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
